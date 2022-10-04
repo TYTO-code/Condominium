512bb993cd30af6ea8796fe7aaa1d79b461c49fa
Igor Nathan Monteiro Santos <igormontcode@gmail.com>
2022-10-04 19:09:48 -0300

    feat: integrate TopBar and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Box, Flex } from '@chakra-ui/react';
+
+import Footer from './components/Footer';
+import TopBar from './components/TopBar';
+
 function App(): JSX.Element {
-  return <h1>Hello, Condominium!</h1>;
+  return (
+    <Flex direction="column" w="100%" h="100vh">
+      <TopBar />
+      <Box w="100%" flex={1}>
+        <h1>Hello, condominium</h1>
+      </Box>
+      <Footer />
+    </Flex>
+  );
 }
 
 export default App;
