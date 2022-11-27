6a788ab5f430b23424bac7bd9c3c8b7f49a570ec
Igor Nathan Monteiro Santos <igornathandev@gmail.com>
2022-11-27 09:41:52 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import { Box, Flex } from '@chakra-ui/react';
 
 import Footer from './components/Footer';
@@ -7,12 +9,31 @@ import Login from './pages/Login';
 import ResidentsPanel from './pages/ResidentsPanel';
 import SyndicPanel from './pages/SyndicPanel';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />
+  },
+  {
+    path: '/painel-associado',
+    element: <ADMPanel />
+  },
+  {
+    path: '/painel-morador',
+    element: <ResidentsPanel />
+  },
+  {
+    path: '/painel-sindico',
+    element: <SyndicPanel />
+  }
+]);
+
 function App(): JSX.Element {
   return (
     <Flex direction="column" w="100%" h="100vh">
       <TopBar />
       <Box w="100%" flex={1}>
-        <Login />
+        <RouterProvider router={router} />
       </Box>
       <Footer />
     </Flex>
